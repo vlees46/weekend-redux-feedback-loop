6aac66edb489f8db4cfdfc0e8fbafdebf22ea97c
Victor Lees <vlees14.pa@gmail.com>
2021-12-03 10:04:05 -0500

    Initial commit

--- README.md
+++ README.md
@@ -7,3 +7,17 @@
 Your project description goes here. What problem did you solve? How did you solve it?
 
 Additional README details can be found [here](https://github.com/PrimeAcademy/readme-template/blob/master/README.md).
+
+
+##HIGH Level Development Plan
+
+1. Create feeling page
+    a. components
+        i. Input field number
+            a. check for validation, user must input a value
+            b. NEXT Button - store user data into array and then calls next FORM
+2. The above logic is for all user supplied input afterwards
+    1. User will be able to review there submissions via - REVIEW COMPONENT
+        a. REVIEW COMPONENT - will have a SUBMIT button which will POST data to database
+            i. After user will see a submission success page - page-five.png
+
--- src/index.js
+++ src/index.js
@@ -3,6 +3,39 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+
+
+let defaultStore = {
+     feeling: ''
+}
+
+
+// Create reducer, manages all change of state from various components
+
+const feedbackReducer = ( state = defaultStore. action) => {
+    if (action type === "ADD_FEELING") {
+        return {...state, feeling: aciton.payload };
+    }
+
+}
+
+const storeInstance = createStore(
+    combineReducers(
+        {
+            feedbackReducer
+        }
+
+    )
+           applyMiddleware(logger)
+)
+
+ReactDOM.render(
+<Provider store={storeInstance}>
+    <App />
+</Provider>
+    , document.getElementById('root'));
 registerServiceWorker();
